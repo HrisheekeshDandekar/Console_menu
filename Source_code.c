88b46e37698ad5ffe974b4f19a8406a3e2ffee8b
HrisheekeshDandekar <hridan8@gmail.com>
2021-06-18 20:59:03 +0530

    Adding a feature to be able to decide how many attributes an Item needs to have out of 3

--- Source_code.c
+++ Source_code.c
@@ -8,10 +8,12 @@
 * 4)print the whole table in one go.
 *
 * ASSUMPTIONS:
-* 1)For now the user can only add 2 items with one related info
+* 1)The format is followd correctly
 */
 
 #include<stdio.h>
+
+//Values used throughout the code. (Adding them before using the tool is necessary)
 #define MAX_BUFFER 200
 #define SINGLE_LEFT_UPPER 218
 #define SINGLE_RIGHT_UPPER 191
@@ -26,9 +28,7 @@
 #define DOUBLE_HORIZONTAL 205
 #define DOUBLE_VERTICAL 186
 
-//int display_menu(struct menu_contents menu);
-//int horizontal_padding_values(int largest_length);
-//int print_the_frame(struct menu_contents menu, int title_len, int item1_len, int price1_len, int item2_len, int price2_len, int largest_length, int horizontal_padd_size);
+//functions which are necessary to be added
 int horizontal_padding(int largest_length);
 int title_printing(struct menu_contents menu, int largest_length);
 int item_display_format(struct item_contents item, int item_full_length, int largest_length);
@@ -36,14 +36,10 @@ int console_menu(struct menu_contents menu, struct item_contents item1, struct i
 	struct item_contents item6, struct item_contents item7, struct item_contents item8, struct item_contents item9, struct item_contents item10);
 
 
-
+//Structs to be copied as they are
 struct menu_contents
 {
 	char title[MAX_BUFFER];
-	/*char item1[MAX_BUFFER];
-	char price1[MAX_BUFFER];
-	char item2[MAX_BUFFER];
-	char price2[MAX_BUFFER];*/
 };
 struct item_contents
 {
@@ -75,61 +71,64 @@ int main()
 	printf("\n%c",DOUBLE_VERTICAL );
 	*/
 
-	struct menu_contents menu =               //Format{Title,Item1,Item2,}
+	/*
+	* FORMAT:
+	* 
+	* For TITLE:
+	* Just one string for title
+	* 
+	* For ITEMS;
+	* "Main name (Printed Leftmost of the row)","Attribute1(Will be to immediate right of Main name)","Attribute2(Will be to immediate right of Attribute1)","Right most attribute(Will be rightmost of the row no matter what)"
+	*/
+
+	struct menu_contents menu =              
 	{
-		//Normal output
-		"FINAF Pizzeria abra ka dabra gili gili chuuuu"/*,"Margaretta Double Cheese with Cheese Burst","Rs.225/-","BlueBerry Cheese Cake","Rs.125/-"*/
-		// Max values to fit the length of console
-		//"a","a","a","a","aaaaaa321321321aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-		//Vales exceeding the max array sixe
-		//"a","a","a","a","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-
-
+		"Title"							//Title of your menu
 	};
-
 	struct item_contents item1=
 	{
-		"Ryzen5 5600x","Clock Rate3700 -  4600 MHz","65watt","Rs. 27,849"
+		"Item1","Attribute1.1","Attribute1.2","Rightmost attribute"		//Item1	
 	};
 	struct item_contents item2 =
 	{
-		//"GTX 3060ti","OC Mode - 1740","600W","Rs. 35,900"
-		"","","",""
+		"Item2","Attribute2.1","Attribute2.2","Rightmost attribute2"						//Item2
+		//"","","",""
 	};
 	struct item_contents item3 =
 	{
-		"AMD B550","PCIe 4.0/3.0 x4 M.2","Q-Flash Plus","Rs. 9,550"
+		"Item3","Attribute3.1","Attribute3.2","Rightmost attribute3"				//Item3
 	};
 	struct item_contents item4 =
 	{
-		"Ryzen5 5600x","Clock Rate 3700 - 4600 MHz","65watt","Rs/-27,849"
+		"Item4","Attribute4.1","Attribute4.2","Rightmost attribute4"		//Item4
 	};
 	struct item_contents item5 =
 	{
-		"Ryzen5 5600x","Clock Rate 3700 - 4600 MHz","65watt","Rs/-27,849"
+		"Item5","Attribute5.1","Attribute5.2","Rightmost attribute5"		//Item5
 	};
 	struct item_contents item6 =
 	{
-		"Ryzen5 5600x","Clock Rate 3700 - 4600 MHz","65watt","Rs/-27,849"
+		"Item6","Attribute6.1","Attribute6.2","Rightmost attribute6"		//Item6
 	};
 	struct item_contents item7 =
 	{
-		"Ryzen5 5600x","Clock Rate 3700 - 4600 MHz","65watt","Rs/-27,849"
+		"Item7","Attribute7.1","Attribute7.2","Rightmost attribute7"		//Item7
 	};
 	struct item_contents item8 =
 	{
-		"Ryzen5 5600x","Clock Rate 3700 - 4600 MHz","65watt","Rs/-27,849"
+		"Item8","Attribute8.1","Attribute8.2","Rightmost attribute8"		//Item8
 	};
 	struct item_contents item9 =
 	{
-		"Ryzen5 5600x","Clock Rate	3700 - 4600 MHz","65watt","Rs/-27,849"
+		"Item9","Attribute9.1","Attribute9.2","Rightmost attribute9"		//Item9
 	};
 	struct item_contents item10 =
 	{
-		"Ryzen5 5600x","Clock Rate	3700 - 4600 MHz","65watt","Rs/-27,849"
+		"Item10","Attribute10.1","Attribute10.2","Rightmost attribute10"		//Item10
 	};
-	//display_menu(menu);		
-	console_menu(menu, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
+		
+	console_menu(menu, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10); //Calling the function to print the contents in a tabular format 
+
 	return 0;
 
 }
@@ -291,6 +290,11 @@ int console_menu(struct menu_contents menu ,struct item_contents item1, struct i
 		printf("\n\n\n");
 		exit();
 	}
+	if (title_len == 0)				//Title is the only value in the menue which cannot be empty.
+	{
+		printf("Title Cannot be empty. Please fill in the slot of the same");
+		exit();
+	}
 
 	//Starting to print the menu
 
@@ -353,29 +357,9 @@ int console_menu(struct menu_contents menu ,struct item_contents item1, struct i
 	printf("%c\n", DOUBLE_RIGHT_LOWER);
 
 
-
-
-
-
-
-
-
-
 	return 0;
 }
-/*int item_display_format(int title_len,int item1_len, int attribute1_1_len, int attribute1_2_len, int attribute_rightmost1_len, int item2_len, int attribute2_1_len,
-	int attribute2_2_len, int attribute_rightmost2_len, int item3_len, int attribute3_1_len, int attribute3_2_len, int attribute_rightmost3_len,int item4_len, int attribute4_1_len, 
-	int attribute4_2_len,int attribute_rightmost4_len, int item5_len, int attribute5_1_len, int attribute5_2_len, int attribute_rightmost5_len, int item6_len, int attribute6_1_len, 
-	int attribute6_2_len, int attribute_rightmost6_len,int item7_len, int attribute7_1_len, int attribute7_2_len, int attribute_rightmost7_len, int item8_len, int attribute8_1_len, 
-	int attribute8_2_len, int attribute_rightmost8_len, int item9_len, int attribute9_1_len, int attribute9_2_len, int attribute_rightmost9_len,int item10_len, int attribute10_1_len, 
-	int attribute10_2_len, int attribute_rightmost10_len,
-	int item1_full_length, int item2_full_length, int item3_full_length, int item4_full_length, int item5_full_length, int item6_full_length, int item7_full_length, int item8_full_length, 
-	int item9_full_length, int item10_full_length,int largest_length,
-	struct menu_contents menu, struct item_contents item1, struct item_contents item2, struct item_contents item3, struct item_contents item4, struct item_contents item5,
-	struct item_contents item6, struct item_contents item7, struct item_contents item8, struct item_contents item9, struct item_contents item10)
-{
-	return 0;
-}*/
+
 int horizontal_padding(int largest_length)
 {
 	//Horizontal length of the console is 120 characters
@@ -391,6 +375,7 @@ int horizontal_padding(int largest_length)
 	{
 		printf(" ");
 	}
+	return 0;
 }
 
 int item_display_format(struct item_contents item, int item_full_length, int largest_length)
@@ -406,29 +391,62 @@ int item_display_format(struct item_contents item, int item_full_length, int lar
 	printf("%c", DOUBLE_VERTICAL);
 
 
-	printf("%c", SINGLE_LEFT_UPPER);							//Start of item name single frame
+	if (item_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_UPPER);							//Start of item name single frame
+	}
+	else
+	{
+		printf("  ");
+	}
+
 	for (counter = 0; counter < item_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_UPPER);
+	if (item_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_UPPER);
+	}
+	
+	if (attribute1_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_UPPER);							//Start of attribute1 single frame
+	}
+	else
+	{
+		printf("  ");
+	}
 
-
-	printf("%c", SINGLE_LEFT_UPPER);							//Start of attribute1 single frame
 	for (counter = 0; counter < attribute1_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_UPPER);
+	if (attribute1_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_UPPER);
+	}
+	
 
+	if (attribute2_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_UPPER);							//Start of attribute2 single frame 
+	}
+	else
+	{
+		printf("  ");
+	}
 
-	printf("%c", SINGLE_LEFT_UPPER);							//Start of attribute2 single frame 
+	
 	for (counter = 0; counter < attribute2_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_UPPER);
-
+	if (attribute2_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_UPPER);
+	}
+	
 
 	if (item_full_length != largest_length)						//Padding betwwen right most value and attribute 2 in case if this is not the largest string
 	{
@@ -439,31 +457,62 @@ int item_display_format(struct item_contents item, int item_full_length, int lar
 	}
 
 
-
-	printf("%c", SINGLE_LEFT_UPPER);							//Start of rightmost attribute single frame 
+	if (attribute_rightmost_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_UPPER);							//Start of rightmost attribute single frame 
+	}
+	else
+	{
+		printf("  ");
+	}
 	for (counter = 0; counter < attribute_rightmost_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_UPPER);
+	if (attribute_rightmost_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_UPPER);
+	}
 
 	printf("%c\n", DOUBLE_VERTICAL);
 	
+
 																	//LINE 2 (Printing strings)
 	horizontal_padding(largest_length);   //Padding to get the table at the centre
 	printf("%c", DOUBLE_VERTICAL);
 
-	printf("%c", SINGLE_VERTICAL);
-	printf("%s", item.item);		//Item name
-	printf("%c", SINGLE_VERTICAL);
-
-	printf("%c", SINGLE_VERTICAL);
-	printf("%s", item.attribute1);		//Attribute1
-	printf("%c", SINGLE_VERTICAL);
-
-	printf("%c", SINGLE_VERTICAL);
-	printf("%s", item.attribute2);		//Attribute2
-	printf("%c", SINGLE_VERTICAL);
+	if (item_len != 0)
+	{
+		printf("%c", SINGLE_VERTICAL);
+		printf("%s", item.item);		//Item name
+		printf("%c", SINGLE_VERTICAL);
+	}
+	else
+	{
+		printf("  ");
+	}
+	
+	if (attribute1_len != 0)
+	{
+		printf("%c", SINGLE_VERTICAL);
+		printf("%s", item.attribute1);		//Attribute1
+		printf("%c", SINGLE_VERTICAL);
+	}
+	else
+	{
+		printf("  ");
+	}
+	
+	if (attribute2_len != 0)
+	{
+		printf("%c", SINGLE_VERTICAL);
+		printf("%s", item.attribute2);		//Attribute2
+		printf("%c", SINGLE_VERTICAL);
+	}
+	else
+	{
+		printf("  ");
+	}
 
 	if (item_full_length != largest_length)						//Padding betwwen right most value and attribute 2 in case if this is not the largest string
 	{
@@ -473,10 +522,17 @@ int item_display_format(struct item_contents item, int item_full_length, int lar
 		}
 	}
 
-	printf("%c", SINGLE_VERTICAL);
-	printf("%s", item.attribute_rightmost);		//Attribute_rightmost
-	printf("%c", SINGLE_VERTICAL);
-
+	if (attribute_rightmost_len != 0)
+	{
+		printf("%c", SINGLE_VERTICAL);
+		printf("%s", item.attribute_rightmost);		//Attribute_rightmost
+		printf("%c", SINGLE_VERTICAL);
+	}
+	else
+	{
+		printf("  ");
+	}
+	
 	printf("%c\n", DOUBLE_VERTICAL);
 
 																//LINE 3 (Closing of all the single borders)
@@ -484,28 +540,61 @@ int item_display_format(struct item_contents item, int item_full_length, int lar
 	printf("%c", DOUBLE_VERTICAL);
 
 
-	printf("%c", SINGLE_LEFT_LOWER);							//End of item name single frame
+	if (item_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_LOWER);							//End of item name single frame
+	}
+	else
+	{
+		printf("  ");
+	}
+
 	for (counter = 0; counter < item_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_LOWER);
+	if (item_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_LOWER);
+	}
 
+	if (attribute1_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_LOWER);							//End of attribute1 single frame
+	}
+	else
+	{
+		printf("  ");
+	}
 
-	printf("%c", SINGLE_LEFT_LOWER);							//End of attribute1 single frame
 	for (counter = 0; counter < attribute1_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_LOWER);
+	if (attribute1_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_LOWER);
+	}
 
 
-	printf("%c", SINGLE_LEFT_LOWER);							//End of attribute2 single frame 
+	if (attribute2_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_LOWER);							//End of attribute2 single frame 
+	}
+	else
+	{
+		printf("  ");
+	}
+
+
 	for (counter = 0; counter < attribute2_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_LOWER);
+	if (attribute2_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_LOWER);
+	}
 
 
 	if (item_full_length != largest_length)						//Padding betwwen right most value and attribute 2 in case if this is not the largest string
@@ -517,16 +606,26 @@ int item_display_format(struct item_contents item, int item_full_length, int lar
 	}
 
 
-
-	printf("%c", SINGLE_LEFT_LOWER);							//End of rightmost attribute single frame 
+	if (attribute_rightmost_len != 0)
+	{
+		printf("%c", SINGLE_LEFT_LOWER);							//End of rightmost attribute single frame 
+	}
+	else
+	{
+		printf("  ");
+	}
 	for (counter = 0; counter < attribute_rightmost_len; counter++)
 	{
 		printf("%c", SINGLE_HORIZONTAL);
 	}
-	printf("%c", SINGLE_RIGHT_LOWER);
+	if (attribute_rightmost_len != 0)
+	{
+		printf("%c", SINGLE_RIGHT_LOWER);
+	}
 
 	printf("%c\n", DOUBLE_VERTICAL);
 
+	return 0;
 }
 
 int title_printing(struct menu_contents menu, int largest_length)
